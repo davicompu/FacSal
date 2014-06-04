a15d785ce8dfcb32134f2490660b0feb3a35398f
chrisjsherm <chris.sherman@outlook.com>
2014-06-04 10:54:37 -0400

    Many-to-many association with checkboxes on edit view for adjustments and salary.

--- Facsal/App/viewmodels/salaries/edit.js
+++ Facsal/App/viewmodels/salaries/edit.js
@@ -12,22 +12,28 @@
 
             rankTypes = ko.observableArray(),
 
-            specialAdjustmentTypes = ko.observableArray(),
-                
-            statusTypes = ko.observableArray();
+            adjustmentTypes = ko.observableArray(),
+
+            statusTypes = ko.observableArray(),
+
+            adjustmentItemVM = {
+                id: ko.observable(),
+                name: ko.observable(),
+                isSelected: ko.observable()
+            };
 
         var vm = {
             activate: activate,
             attached: attached,
 
+            adjustmentVMs: ko.observableArray(),
             appointmentTypes: appointmentTypes,
             facultyTypes: facultyTypes,
             meritAdjustmentTypes: meritAdjustmentTypes,
             rankTypes: rankTypes,
             salary: ko.observable(),
             salaryId: ko.observable(),
-            selectedSpecialAdjustmentTypes: ko.observableArray(),
-            specialAdjustmentTypes: specialAdjustmentTypes,
+            adjustmentTypes: adjustmentTypes,
             statusTypes: statusTypes,
 
             saveChanges: saveChanges,
@@ -68,21 +74,34 @@
                     self.rankTypes(response);
                 });
 
-            var predicate = new breeze.Predicate('id', '==', vm.salaryId());
-            var expansionProperties = 'person, specialSalaryAdjustments';
-            var salary = unitofwork.salaries.find(predicate, expansionProperties)
+            var adjustmentTypes = unitofwork.specialAdjustmentTypes.all()
                 .then(function (response) {
-                    vm.salary(response[0]);
-                });
-
-            var specialAdjustmentTypes = unitofwork.specialAdjustmentTypes.all()
-                .then(function (response) {
-                    self.specialAdjustmentTypes(response);
+                    self.adjustmentTypes(response);
                 });
 
             var statusTypes = unitofwork.statusTypes.all()
                 .then(function (response) {
                     self.statusTypes(response);
+                });
+
+            var predicate = new breeze.Predicate('id', '==', vm.salaryId());
+            var expansionProperties = 'person, specialSalaryAdjustments';
+            var salary = unitofwork.salaries.find(predicate, expansionProperties)
+                .then(function (response) {
+                    var salary = response[0];
+
+                    var adjustmentHash = createSalaryAdjustmentHash(salary);
+
+                    var adjustmentMapVMs = $.map(self.adjustmentTypes(), function (adjustment) {
+                        return {
+                            adjustment: adjustment,
+                            isSelected: ko.observable(!!adjustmentHash[adjustment.id()])
+                        };
+                    });
+
+                    vm.adjustmentVMs(adjustmentMapVMs)
+
+                    return vm.salary(salary);
                 });
 
             Q.all([
@@ -91,7 +110,7 @@
                 meritAdjustmentTypes,
                 rankTypes,
                 salary,
-                specialAdjustmentTypes,
+                adjustmentTypes,
                 statusTypes
             ]).fail(self.handleError);
 
@@ -101,27 +120,57 @@
         function saveChanges() {
             var self = this;
 
-            evaluateSelectedSpecialAdjustmentTypesArray();
+            applySelectionsToSalaryAdjustmentMaps();
 
             if (!unitofwork.hasChanges()) {
-                return true;
+                return logger.log('No changes were detected.', null, null, true);
             }
 
             unitofwork.commit()
                 .then(function () {
-                    logger.logSuccess('Save successful', null, null, true);
+                    return logger.logSuccess('Save successful', null, null, true);
                 })
                 .fail(self.handleError);
 
             return true;
         }
 
-        function evaluateSelectedSpecialAdjustmentTypesArray() {
-            $.each(vm.selectedSpecialAdjustmentTypes(), function (index, value) {
-                var adjustment = unitofwork.specialSalaryAdjustments.create({
-                    salaryId: vm.salaryId(),
-                    specialAdjustmentTypeId: value
-                });
+        function createSalaryAdjustmentHash(salary) {
+            var adjustmentHash = {};
+
+            $.each(salary.specialSalaryAdjustments(), function (index, map) {
+                adjustmentHash[map.specialAdjustmentTypeId()] = map;
+            });
+
+            return adjustmentHash;
+        }
+
+        function applySelectionsToSalaryAdjustmentMaps() {
+            var salary = vm.salary(),
+
+                mapVMs = vm.adjustmentVMs(),
+
+                adjustmentHash = createSalaryAdjustmentHash(salary);
+
+            $.each(mapVMs, function (index, mapVM) {
+                var map = adjustmentHash[mapVM.adjustment.id()];
+
+                if (mapVM.isSelected()) {
+                    // User selected this adjustment.
+                    if (!map) {
+                        // No existing map, so create one.
+                        map = unitofwork.specialSalaryAdjustments.create({
+                            salaryId: salary.id(),
+                            specialAdjustmentTypeId: mapVM.adjustment.id()
+                        });
+                    }
+                } else {
+                    // User unselected this adjustment.
+                    if (map) {
+                        // If map exists, delete it.
+                        map.entityAspect.setDeleted();
+                    }
+                }
             });
         }
     });
