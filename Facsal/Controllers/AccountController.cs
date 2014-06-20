ad8caa64d3e2ace82026556139d9f1e3364ae6f8
chrisjsherm <chris.sherman@outlook.com>
2014-06-20 15:05:09 -0400

    In-progress commit. Need to call AccountController from services/security.

---
+++ Facsal/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using Facsal.Models;
+using SalaryEntities.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Facsal.Controllers
+{
+    public class AccountController : ApiController
+    {
+        IUnitOfWork UnitOfWork;
+
+        public AccountController (IUnitOfWork unitOfWork)
+        {
+            UnitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public HttpResponseMessage GetUserInfo()
+        {
+            var userInfo = new UserInfoViewModel
+            {
+                UserName = User.Identity.Name,
+            };
+
+            var rolesNames = UnitOfWork.RoleAssignmentRepository
+                .Find(ra => ra.User.Pid == User.Identity.Name)
+                .Select(ra => ra.Role.Name);
+
+            userInfo.UserRoles = string.Join(",", rolesNames);
+
+            return Request.CreateResponse<UserInfoViewModel>(HttpStatusCode.OK, userInfo);
+        }
+    }
+}
