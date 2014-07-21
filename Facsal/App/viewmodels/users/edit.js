d125495bff2d8257e8f71f213ac6e09fb8f4fe11
chrisjsherm <chris.sherman@outlook.com>
2014-07-21 15:47:34 -0400

    Edit user view and viewmodel.

--- Facsal/App/viewmodels/users/edit.js
+++ Facsal/App/viewmodels/users/edit.js
@@ -31,31 +31,18 @@
 
                 roles = unitofwork.roles.find(p1)
                     .then(function (response) {
-                        var roles = response;
+                        var roles = response,
 
-                        var p1 = new breeze.Predicate('id', '==', vm.userId()),
+                            user = vm.user(),
 
-                            p2 = new breeze.Predicate('roleAssignments.role.departmentId', '==', newValue),
+                            assignmentHash = createRoleAssignmentHash(user),
 
-                            predicate = breeze.Predicate.and([p1, p2]),
-
-                            expansionCondition = 'roleAssignments',
-
-                            user = unitofwork.users.find(predicate, expansionCondition)
-                                .then(function (response) {
-                                    var user = response[0],
-                                        assignmentHash = createRoleAssignmentHash(user),
-                                        assignmentMapVMs = $.map(roles, function (role) {
-                                            return {
-                                                role: role,
-                                                isSelected: ko.observable(!!assignmentHash[role.id])
-                                            };
-                                        });
-
-                                    vm.assignmentVMs(assignmentMapVMs);
-
-                                    return vm.user(user);
-                                });
+                            assignmentMapVMs = $.map(roles, function (role) {
+                                return {
+                                    role: role,
+                                    isSelected: ko.observable(!!assignmentHash[role.id()])
+                                };
+                            });
 
                         vm.assignmentVMs(assignmentMapVMs);
                     })
@@ -98,57 +85,34 @@
 
         function activate(userId) {
             ga('send', 'pageview', { 'page': window.location.href, 'title': document.title });
+
             vm.userId(userId);
+
             return true;
         }
 
         function attached() {
-            var self = this,
+            var predicate = new breeze.Predicate('id', '==', vm.userId()),
+
+                expansionCondition = 'roleAssignments',
+
+                user = unitofwork.users.find(predicate, expansionCondition)
+                    .then(function (response) {
+                        vm.user(response[0]);
+                    }),
 
                 units = unitofwork.manageableUnits.all()
                     .then(function (response) {
                         vm.units(response);
                     });
 
-            //    roles = unitofwork.getAssignableRoles()
-            //        .then(function (response) {
-            //            vm.roles(response);
-            //        });
-
-            //var p1 = new breeze.Predicate('id', '==', vm.userId()),
-
-            //    p2 = new breeze.predicate('roleAssignments.role.departmentId', '==', newValue),
-
-            //    predicate = breeze.Predicate.and([p1, p2]),
-
-            //    expansionCondition = 'roleAssignments',
-
-            //    user = unitofwork.users.find(predicate, expansionCondition)
-            //        .then(function (response) {
-            //            var user = response[0],
-            //                assignmentHash = createRoleAssignmentHash(user),
-            //                assignmentMapVMs = $.map(vm.roles(), function (role) {
-            //                    return {
-            //                        role: role,
-            //                        isSelected: ko.observable(!!assignmentHash[role.id])
-            //                    };
-            //                });
-
-            //            vm.assignmentVMs(assignmentMapVMs);
-
-            //            return vm.user(user);
-            //        });
-
-            //Q.all([
-            //    roles,
-            //    user
-            //]).fail(self.handleError);
-
             return true;
         }
 
         function deactivate() {
             vm.user(undefined);
+            vm.units(undefined);
+            return true;
         }
 
         function saveChanges() {
@@ -163,7 +127,6 @@
             unitofwork.commit()
                 .then(function (response) {
                     return logger.logSuccess('Save successful', response, system.getModuleId(vm), true);
-                    //return router.navigateBack();
                 })
                 .fail(self.handleError);
 
@@ -191,14 +154,14 @@
                 assignmentHash = createRoleAssignmentHash(user);
 
             $.each(mapVMs, function (index, mapVM) {
-                var map = assignmentHash[mapVM.role.id];
+                var map = assignmentHash[mapVM.role.id()];
 
                 if (mapVM.isSelected()) {
                     // User selected this assignment.
                     if (!map) {
                         // No existing map, so create one.
                         map = unitofwork.roleAssignments.create({
-                            roleId: mapVM.role.id,
+                            roleId: mapVM.role.id(),
                             userId: user.id()
                         });
                     }
